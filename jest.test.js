9f3255876e46c4e12a44e00136abb49210763b19
Carlos Pires <extraterra1@gmail.com>
2023-10-06 11:10:01 +0100

    add calculator

--- jest.test.js
+++ jest.test.js
@@ -47,6 +47,24 @@ test('subtract', () => {
 test('divide', () => {
   expect(calculator.divide(2, 2)).toBe(1);
 });
+test('random', () => {
+  const randomA = Math.floor(Math.random() * 101);
+  const randomB = Math.floor(Math.random() * 101);
+
+  expect(calculator.sum(randomA, randomB)).toBe(randomA + randomB);
+  expect(calculator.multiply(randomA, randomB)).toBe(randomA * randomB);
+  expect(calculator.divide(randomA, randomB)).toBe(randomA / randomB);
+  expect(calculator.subtract(randomA, randomB)).toBe(randomA - randomB);
+});
 test('not a number', () => {
   expect(() => calculator.sum('xd', 2)).toThrow('all inputs must be numbers');
+  expect(() => calculator.subtract('xd', 2)).toThrow('all inputs must be numbers');
+  expect(() => calculator.multiply('xd', 2)).toThrow('all inputs must be numbers');
+  expect(() => calculator.divide('xd', 2)).toThrow('all inputs must be numbers');
+});
+test('not a number 2', () => {
+  expect(() => calculator.sum(2, 'xd')).toThrow('all inputs must be numbers');
+});
+test('no divide 0', () => {
+  expect(() => calculator.divide(2, 0)).toThrow("you can't divide by zero");
 });
