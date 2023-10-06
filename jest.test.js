31a4d9ae1ddeed2bdccb011e067c86ff63931cc2
Carlos Pires <extraterra1@gmail.com>
2023-10-06 10:54:24 +0100

    add reverseString

--- jest.test.js
+++ jest.test.js
@@ -14,3 +14,39 @@ test('empty', () => {
 test('single character', () => {
   expect(capitalize('b')).toBe('B');
 });
+
+const reverseString = require('./reverseString');
+
+test('reverse', () => {
+  expect(reverseString('this is a test')).toBe('tset a si siht');
+});
+
+test('reverse number', () => {
+  expect(reverseString('23')).toBe('32');
+});
+
+test('single letter', () => {
+  expect(reverseString('t')).toBe('t');
+});
+
+test('not string', () => {
+  expect(() => reverseString(23)).toThrow('input must be a string');
+});
+
+const calculator = require('./calculator');
+
+test('sum', () => {
+  expect(calculator.sum(2, 2)).toBe(4);
+});
+test('multiply', () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+});
+test('subtract', () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
+test('divide', () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+});
+test('not a number', () => {
+  expect(() => calculator.sum('xd', 2)).toThrow('all inputs must be numbers');
+});
